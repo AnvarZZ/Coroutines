a61c7443e45f2cbc33aad5ec41e9bb21a72f10f7
AnvarZZ <anvarzeynalzade@gmail.com>
2022-11-07 14:55:33 +0400

    Suspend functions were added

--- app/src/main/java/az/anvar/coroutines/MainActivity.kt
+++ app/src/main/java/az/anvar/coroutines/MainActivity.kt
@@ -1,11 +1,53 @@
 package az.anvar.coroutines
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
+import az.anvar.coroutines.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.buttonLoad.setOnClickListener {
+            lifecycleScope.launch {
+                loadData()
+            }
+        }
+    }
+
+    private suspend fun loadData() {
+        binding.progress.isVisible = true
+        binding.buttonLoad.isEnabled = false
+        val city = loadCity()
+        binding.tvLocation.text = city
+        val temp = loadTemperature(city)
+        binding.tvTemperature.text = temp.toString()
+        binding.progress.isVisible = false
+        binding.buttonLoad.isEnabled = true
+    }
+
+    private suspend fun loadCity(): String {
+        delay(5000)
+        return "Baki"
+    }
+
+    private suspend fun loadTemperature(city: String): Int {
+        Toast.makeText(
+            this,
+            getString(R.string.loading_temperature_toast, city),
+            Toast.LENGTH_SHORT
+        ).show()
+        delay(5000)
+        return 13
     }
 }
